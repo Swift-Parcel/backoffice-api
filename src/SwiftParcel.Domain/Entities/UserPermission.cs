51c705d99bcb3c7dcb51598e84f1cc1e321e2805
Barnabas Kiss <barnikissbarni@gmail.com>
2026-07-27 14:08:32 +0200

    Created UserPermission Model

---
+++ src/SwiftParcel.Domain/Entities/UserPermission.cs
@@ -0,0 +1,11 @@
+namespace SwiftParcel.Domain.Entities;
+
+public class UserPermission
+{
+    public int UserId { get; set; }
+    public int PermissionId { get; set; }
+    public DateTime Expires { get; set; }
+    
+    public User? User { get; set; }
+    public Permission? Permission { get; set; }
+}
