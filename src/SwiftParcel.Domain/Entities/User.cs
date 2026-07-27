c802a31c291a5e9c17cbf0a0fcd308cffee1e845
Barnabas Kiss <barnikissbarni@gmail.com>
2026-07-27 14:08:18 +0200

    Created User Model

---
+++ src/SwiftParcel.Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+namespace SwiftParcel.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public bool IsActive { get; set; }
+    public DateTime LastLogin { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int CreatedBy { get; set; }
+}
