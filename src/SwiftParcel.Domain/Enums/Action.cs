f7133e76f3e80291cbb5defa92431cad33c9d5ce
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-27 11:12:59 +0200

    add: create Action enum

---
+++ src/SwiftParcel.Domain/Enums/Action.cs
@@ -0,0 +1,16 @@
+namespace SwiftParcel.Domain.Enums;
+
+public enum Action
+{
+    Create,
+    StatusChange,
+    Assign,
+    Update,
+    PermissionGrant,
+    LoginFailed,
+    LoginSucceeded,
+    RoleChange,
+    Delete,
+    Escalate,
+    NodeAdd
+}
