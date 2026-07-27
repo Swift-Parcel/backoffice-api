45ceb2ae07ff6982f6f5dfa8a86c8eb10ba8af5c
Barnabas Kiss <barnikissbarni@gmail.com>
2026-07-27 13:51:59 +0200

    Created StatusWorkflow Model

---
+++ src/SwiftParcel.Domain/Entities/StatusWorkflow.cs
@@ -0,0 +1,13 @@
+namespace SwiftParcel.Domain.Entities;
+
+using Enums;
+
+public class StatusWorkflow
+{
+    public int Id { get; set; }
+    public CaseStatus? FromStatus { get; set; }
+    public CaseStatus? ToStatus { get; set; }
+    public bool RequireNote { get; set; }
+    public bool RequireResolution { get; set; }
+    public bool IsActive { get; set; }
+}
