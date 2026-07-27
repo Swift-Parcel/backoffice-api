fe29c641d675b2dead6a0498353b7fb29e28647c
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-27 11:14:18 +0200

    add: create EntityType enum

---
+++ src/SwiftParcel.Domain/Enums/EntityType.cs
@@ -0,0 +1,20 @@
+namespace SwiftParcel.Domain.Enums;
+
+public enum EntityType
+{
+    AutoAssignmentRule,
+    Note,
+    Case,
+    Customer,
+    EmailTemplate,
+    Handler,
+    Holiday,
+    Parcel,
+    Region,
+    Role,
+    SlaRule,
+    StatusWorkflow,
+    SystemConfig,
+    UserPermission,
+    User
+}
