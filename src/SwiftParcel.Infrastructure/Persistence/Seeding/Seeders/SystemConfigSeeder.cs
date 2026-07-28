216be370a2503e7a3c333df0955ea125511c4b31
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-28 10:25:38 +0200

    feat: add seeder files

---
+++ src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/SystemConfigSeeder.cs
@@ -0,0 +1,8 @@
+using SwiftParcel.Infrastructure.Persistence.Seeding.Interfaces;
+
+namespace SwiftParcel.Infrastructure.Persistence.Seeding.Seeders;
+
+public class SystemConfigSeeder : IEntitySeeder
+{
+    
+}
