e7726855a674399cec81240e2b36d2f4c4397dc6
Papp Csongor <csoni2002@gmail.com>
2026-07-28 18:39:46 +0200

    chore: add empty implementations to all seeder class
     so the solution can be built

--- src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/HandlerSeeder.cs
+++ src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/HandlerSeeder.cs
@@ -4,5 +4,10 @@ namespace SwiftParcel.Infrastructure.Persistence.Seeding.Seeders;
 
 public class HandlerSeeder : IEntitySeeder
 {
-    
+    public int Order => 0;
+
+    public Task SeedAsync(AppDbContext dbContext, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
 }
