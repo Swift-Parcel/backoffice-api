bbaecb77d15b4d3c78810277421fbb259afe99fe
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-28 11:08:54 +0200

    feat: create TimestampParserHelper class

--- src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/SystemConfigSeeder.cs
+++ src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/SystemConfigSeeder.cs
@@ -4,5 +4,9 @@ namespace SwiftParcel.Infrastructure.Persistence.Seeding.Seeders;
 
 public class SystemConfigSeeder : IEntitySeeder
 {
-    
+    public int Order => 20;
+    public Task SeedAsync(AppDbContext dbContext, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
 }
