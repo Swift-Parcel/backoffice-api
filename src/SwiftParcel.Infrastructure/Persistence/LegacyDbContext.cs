fed651e9196ea12fbccf897eeff339d7d93eec0f
Papp Csongor <csoni2002@gmail.com>
2026-07-28 18:37:46 +0200

    fix: create missing constructor for legacyDbContext

--- src/SwiftParcel.Infrastructure/Persistence/LegacyDbContext.cs
+++ src/SwiftParcel.Infrastructure/Persistence/LegacyDbContext.cs
@@ -4,5 +4,8 @@ namespace SwiftParcel.Infrastructure.Persistence;
 
 public class LegacyDbContext:DbContext
 {
-    
+    public LegacyDbContext(DbContextOptions<LegacyDbContext> options)
+        : base(options)
+    {
+    }
 }
