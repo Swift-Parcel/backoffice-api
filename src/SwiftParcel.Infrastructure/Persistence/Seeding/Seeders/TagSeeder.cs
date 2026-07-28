a198e475ee3e591fb667c6d4e32bc0c9a412690f
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-28 16:17:48 +0200

    fix: remove redundant line

--- src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/TagSeeder.cs
+++ src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/TagSeeder.cs
@@ -24,7 +24,6 @@ public class TagSeeder : IEntitySeeder
             .ToHashSet();
     }
 
-
     private static IEnumerable<string> CleanTagList(IEnumerable<string> tags)
     {
         var cleanedTags = new List<string>();
