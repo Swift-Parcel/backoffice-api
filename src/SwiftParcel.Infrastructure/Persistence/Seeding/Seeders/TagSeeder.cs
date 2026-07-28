b8ef5c647199be9ebb363c277e949282d2bb1af6
Tamás Horneczki <t.horneczki@tcs.com>
2026-07-28 15:27:56 +0200

    feat: initialize TagSeeder

--- src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/TagSeeder.cs
+++ src/SwiftParcel.Infrastructure/Persistence/Seeding/Seeders/TagSeeder.cs
@@ -1,8 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using SwiftParcel.Domain.Entities;
+using SwiftParcel.Infrastructure.Persistence.Seeding.Helpers;
 using SwiftParcel.Infrastructure.Persistence.Seeding.Interfaces;
 
 namespace SwiftParcel.Infrastructure.Persistence.Seeding.Seeders;
 
 public class TagSeeder : IEntitySeeder
 {
+    public int Order => 19;
+
+    private static IEnumerable<string> CleanTagList(IEnumerable<string> tags)
+    {
+        foreach (var tag in tags)
+        {
+            
+        }
+    }
     
+    public async Task SeedAsync(AppDbContext dbContext, CancellationToken cancellationToken = default)
+    {
+        if (await dbContext.Tags.AnyAsync(cancellationToken))
+        {
+            return;
+        }
+        
+        var legacyConfigs = await dbContext.Database
+            .SqlQueryRaw<LegacyConfigDto>("SELECT tags FROM cases")
+            .ToListAsync(cancellationToken);
+        
+        var newConfigs = new List<LegacyConfigDto>();
+        
+        foreach (var legacyConfig in legacyConfigs)
+        {
+            var legacyTags = StringParserHelper.ParseCsvString(legacyConfig.tags);
+            var cleanTags = CleanTagList(legacyTags);
+            foreach (var cleanTag in cleanTags)
+            {
+                var newConfig = new Tag
+                {
+                    // Id: give incremental id automatically? idk how
+                    Name = cleanTag
+                };
+                newConfigs.Add(newConfig);
+            }
+        }
+        
+        await dbContext.Tags.AddRangeAsync(newConfigs, cancellationToken);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+    
+    private record LegacyConfigDto(
+        string tags);
 }
