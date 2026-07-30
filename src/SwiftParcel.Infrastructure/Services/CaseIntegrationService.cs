78f761d35da457c744b6baf3941e2e54ec1ec990
Barnabas Kiss <barnikissbarni@gmail.com>
2026-07-30 17:00:17 +0200

    feature: CreateCaseAsync

--- src/SwiftParcel.Infrastructure/Services/CaseIntegrationService.cs
+++ src/SwiftParcel.Infrastructure/Services/CaseIntegrationService.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SwiftParcel.Application.DTO.Cases;
+using SwiftParcel.Domain.Entities;
+using SwiftParcel.Domain.Enums;
 
 namespace SwiftParcel.Infrastructure.Services;
 
@@ -46,9 +48,63 @@ public class CaseIntegrationService : ICaseIntegrationService
         return new CustomerCasesResponse(cases);
     }
 
-    public Task<CreateCaseResponse?> CreateCaseAsync(CreateCaseRequest request, CancellationToken cancellationToken = default)
+    public async Task<CreateCaseResponse?> CreateCaseAsync(CreateCaseRequest request, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        var customer = await  _dbcontext.Customers
+            .FirstOrDefaultAsync(c => c.Email == request.CustomerEmail, cancellationToken);
+        
+        if (customer == null)
+            return null;
+
+        var lastcase = await _dbcontext.Cases.OrderByDescending(c => c.CaseNumber).FirstOrDefaultAsync(cancellationToken);
+        int nextSequence = 1;
+        var currentYear = DateTime.UtcNow.Year.ToString(); // "2026" (vagy amilyen év van)
+
+        if (lastcase is not null && !string.IsNullOrEmpty(lastcase.CaseNumber))
+        {
+            var parts = lastcase.CaseNumber.Split('-');
+    
+            if (parts.Length > 2)
+            {
+                string lastYear = parts[1];
+        
+                if (int.TryParse(parts[2], out int lastParsedNumber))
+                {
+                    if (lastYear == currentYear)
+                        nextSequence = lastParsedNumber + 1;
+                    else
+                        nextSequence = 1;
+                }
+            }
+        }
+
+        var caseNumber = $"CASE-{currentYear}-{nextSequence:D3}";
+        var now =  DateTime.UtcNow;
+
+        var slaRule = await _dbcontext.SlaRules
+            .FirstOrDefaultAsync(r => r.IsActive && r.CaseType == request.CaseType, cancellationToken);
+
+        int slaHours = slaRule?.SlaHours ?? 24;
+        var slaDeadline = now.AddHours(slaHours);
+        
+        var caseEntity = new Case
+        {
+            CaseNumber = caseNumber,
+            CustomerId = customer.Id,
+            CaseType = request.CaseType,
+            Status = CaseStatus.Open,
+            Description = request.Description,
+            CreatedDate = now,
+            UpdatedDate = now,
+            SlaDeadline = slaDeadline,
+            Resolution = string.Empty
+        };
+        
+        _dbcontext.Cases.Add(caseEntity);
+        await _dbcontext.SaveChangesAsync(cancellationToken);
+
+        return new CreateCaseResponse(caseNumber);
+
     }
 
     public Task AddCaseNoteAsync(string caseNumber, AddCaseNoteRequest request, CancellationToken cancellationToken = default)
