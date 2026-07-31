fe11cf01e942df6f0714fd3a13d574a41a93e4f0
Barnabas Kiss <barnikissbarni@gmail.com>
2026-07-31 13:58:02 +0200

    fix: CaseIntegrationService

--- src/SwiftParcel.Application/DTO/Cases/CaseCreateRequest.cs
+++ src/SwiftParcel.Application/DTO/Cases/CaseCreateRequest.cs
@@ -7,5 +7,8 @@ public record CreateCaseRequest(
     [property: JsonPropertyName("customer_email")] string CustomerEmail,
     [property: JsonPropertyName("tracking_numbers")] List<string> TrackingNumbers,
     [property: JsonPropertyName("case_type")] CaseType CaseType,
+    [property: JsonPropertyName("case_title")] string CaseTitle,
+    [property: JsonPropertyName("region_id")] int RegionId,
+    [property: JsonPropertyName("channel")] Channel Channel,
     [property: JsonPropertyName("description")] string Description
 );
