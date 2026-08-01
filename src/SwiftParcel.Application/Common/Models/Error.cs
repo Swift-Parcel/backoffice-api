9c85c05440b3ee350dfe44b1d09dae4242e2fb0e
Papp Csongor <csoni2002@gmail.com>
2026-08-01 17:57:43 +0200

    feat: add error class

---
+++ src/SwiftParcel.Application/Common/Models/Error.cs
@@ -0,0 +1,24 @@
+namespace SwiftParcel.Application.Common.Models;
+
+public record Error(string Code, string Description, ErrorType Type)
+{
+    public static Error NotFound(string code, string description) 
+        => new(code, description, ErrorType.NotFound);
+    
+    public static Error Validation(string code, string description) 
+        => new(code, description, ErrorType.Validation);
+    
+    public static Error Conflict(string code, string description) 
+        => new(code, description, ErrorType.Conflict);
+    
+    public static Error Failure(string code, string description) 
+        => new(code, description, ErrorType.Failure);
+}
+
+public enum ErrorType
+{
+    Failure = 0,
+    Validation = 1,
+    NotFound = 2,
+    Conflict = 3
+}
