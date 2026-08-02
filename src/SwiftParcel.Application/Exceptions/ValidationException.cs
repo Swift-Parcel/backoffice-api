41613fa06a3332aafc157761ce4543ce8c8d64c7
Papp Csongor <csoni2002@gmail.com>
2026-08-02 12:57:57 +0200

    feat: extend validation exception
    add a constructor which accepts iEnumerable of errors

--- src/SwiftParcel.Application/Exceptions/ValidationException.cs
+++ src/SwiftParcel.Application/Exceptions/ValidationException.cs
@@ -1,9 +1,21 @@
+using FluentValidation.Results;
+
 namespace SwiftParcel.Application.Exceptions;
 
 public class ValidationException : Exception
 {
     public IDictionary<string, string[]> Errors { get; }
-    
+
+    public ValidationException(IEnumerable<ValidationFailure> failures)
+        : base("One or more validation errors occurred.")
+    {
+        Errors = failures
+            .GroupBy(f => f.PropertyName)
+            .ToDictionary(
+                g => g.Key,
+                g => g.Select(f => f.ErrorMessage).ToArray());
+    }
+
     public ValidationException(IDictionary<string, string[]> errors)
         : base("One or more validation errors occurred.")
     {
