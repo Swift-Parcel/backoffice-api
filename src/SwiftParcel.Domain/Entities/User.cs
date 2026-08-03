c93293d22c8cbf7fc5c20dd40a7bb39650c87441
Tamás Horneczki <t.horneczki@tcs.com>
2026-08-03 12:23:43 +0200

    fix: remove optional entities and seeders for now

--- src/SwiftParcel.Domain/Entities/User.cs
+++ src/SwiftParcel.Domain/Entities/User.cs
@@ -10,8 +10,6 @@ public class User
     public Role Role { get; set; } = null!;
 
     public ICollection<Region> Regions { get; set; }= new List<Region>();
-    
-    public ICollection<UserPermission> Permissions { get; set; } = new List<UserPermission>();
     public string Email { get; set; } = string.Empty;
     public DateTime LastLogin { get; set; }
     public DateTime CreatedDate { get; set; }
