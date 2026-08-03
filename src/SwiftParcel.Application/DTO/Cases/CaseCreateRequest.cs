771e0061e579825bda22884b99b58619d424c982
Barnabas Kiss <barnikissbarni@gmail.com>
2026-08-03 12:09:41 +0200

    fix: more records in CreateCaseRequest

--- src/SwiftParcel.Application/DTO/Cases/CaseCreateRequest.cs
+++ src/SwiftParcel.Application/DTO/Cases/CaseCreateRequest.cs
@@ -6,7 +6,9 @@ namespace SwiftParcel.Application.DTO.Cases;
 public record CreateCaseRequest(
      string CustomerEmail,
      List<string> TrackingNumbers,
+     int RegionId,
      CaseType CaseType,
+     string CaseTitle,
      Channel Channel,
      string Description
 );
