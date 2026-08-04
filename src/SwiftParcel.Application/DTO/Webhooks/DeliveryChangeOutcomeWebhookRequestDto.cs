0a622d579628d3f05be73d906d09da007a9c3a8d
Barnabas Kiss <barnikissbarni@gmail.com>
2026-08-04 19:13:36 +0200

    feature: Dto Email

--- src/SwiftParcel.Application/DTO/Webhooks/DeliveryChangeOutcomeWebhookRequestDto.cs
+++ src/SwiftParcel.Application/DTO/Webhooks/DeliveryChangeOutcomeWebhookRequestDto.cs
@@ -4,6 +4,7 @@ using SwiftParcel.Domain.Enums;
 namespace SwiftParcel.Application.DTO.Webhooks;
 
 public record DeliveryChangeOutcomeWebhookRequestDto(
+     string Email,
      string CaseNumber,
      DeliveryChangeOutcome Outcome
 );
