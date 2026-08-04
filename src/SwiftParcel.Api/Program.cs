96bbe26a22a5dc26f5cba91631fde8b3c48cfc9e
Papp Csongor <csoni2002@gmail.com>
2026-08-04 12:39:53 +0200

    refactor: extract infrastructure registration to its own DI file

--- src/SwiftParcel.Api/Program.cs
+++ src/SwiftParcel.Api/Program.cs
@@ -7,55 +7,16 @@ using SwiftParcel.Application.Common.Interfaces;
 using SwiftParcel.Application.Integration.Interfaces;
 using SwiftParcel.Application.Services;
 using SwiftParcel.Infrastructure.Persistence;
-using SwiftParcel.Domain.Enums;
 using SwiftParcel.Infrastructure;
 using SwiftParcel.Infrastructure.Persistence.Seeding;
-using SwiftParcel.Infrastructure.Persistence.Seeding.Interfaces;
 using SwiftParcel.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 builder.Services.AddProblemDetails();
-
-//dbContext registration
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options
-        .UseNpgsql(connectionString, npgsqlOptions =>
-        {
-            npgsqlOptions.MapEnum<ParcelStatus>("enum_parcel_status");
-            npgsqlOptions.MapEnum<Timeslot>("enum_timeslot");
-            npgsqlOptions.MapEnum<ServiceType>("enum_service_type");
-            npgsqlOptions.MapEnum<CaseType>("enum_case_type");
-            npgsqlOptions.MapEnum<CaseStatus>("enum_case_status");
-            npgsqlOptions.MapEnum<Priority>("enum_priority");
-            npgsqlOptions.MapEnum<Channel>("enum_channel");
-            npgsqlOptions.MapEnum<DayOfWeek>("enum_day_of_week");
-            npgsqlOptions.MapEnum<SwiftParcel.Domain.Enums.AuditAction>("enum_action");
-            npgsqlOptions.MapEnum<EntityType>("enum_entity_type");
-        })
-        .UseSnakeCaseNamingConvention()
-);
-
-builder.Services.AddScoped<IAppDbContext>(sp => sp.GetRequiredService<AppDbContext>());
-
-var legacyConnectionString = builder.Configuration.GetConnectionString("LegacyConnection");
-builder.Services.AddDbContext<LegacyDbContext>(options =>
-    options.UseNpgsql(legacyConnectionString));
-
-//seeder registration
-var seederTypes = typeof(DataSeederOrchestrator).Assembly
-    .GetTypes()
-    .Where(t => typeof(IEntitySeeder).IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
-
-foreach (var type in seederTypes)
-{
-    builder.Services.AddScoped(typeof(IEntitySeeder), type);
-}
-
-builder.Services.AddScoped<DataSeederOrchestrator>();
 
 builder.Services.AddApplication();
 
@@ -91,7 +52,6 @@ builder.Services.AddScoped<IDeliveryEstimationService, DeliveryEstimationService
         client.BaseAddress = new Uri(builder.Configuration["JavaBackend:BaseUrl"]);
     });
 
-    builder.Services.AddInfrastructure(builder.Configuration);
 
     var app = builder.Build();
     app.UseExceptionHandler();
