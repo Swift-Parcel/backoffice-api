172fca0b43848224d1fb8b2ffdf4bab449baa519
Papp Csongor <csoni2002@gmail.com>
2026-08-05 09:46:12 +0200

    add migration for case and customer changes

---
+++ src/SwiftParcel.Infrastructure/Migrations/20260805074447_ChangeCustomerAndCase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwiftParcel.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeCustomerAndCase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "case_number_seq",
+                startValue: 1000L);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "updated_date",
+                table: "cases",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "case_number_seq");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "updated_date",
+                table: "cases",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
