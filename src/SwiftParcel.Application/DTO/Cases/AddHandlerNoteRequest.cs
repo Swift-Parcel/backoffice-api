9c451193b95c2bbfa6bfd414c759aff841c693c0
Tamás Horneczki <t.horneczki@tcs.com>
2026-08-05 17:25:45 +0200

    fix: allow null values in case requests

--- src/SwiftParcel.Application/DTO/Cases/AddHandlerNoteRequest.cs
+++ src/SwiftParcel.Application/DTO/Cases/AddHandlerNoteRequest.cs
@@ -3,5 +3,5 @@ namespace SwiftParcel.Application.DTO.Cases;
 public record AddHandlerNoteRequest(
     string Message, 
     bool IsInternal, 
-    string? Attachment
+    string? Attachment = null
 );
