ac7acf77f9e8789b1cc3b7373188cd4eb8c40d6d
Papp Csongor <csoni2002@gmail.com>
2026-08-05 13:18:14 +0200

    refactor: reorder http methods

--- src/SwiftParcel.Api/Controllers/CasesController.cs
+++ src/SwiftParcel.Api/Controllers/CasesController.cs
@@ -15,6 +15,24 @@ namespace SwiftParcel.Api.Controllers;
 [Authorize]
 public class CasesController : ApiController
 {
+    /// <summary>
+    /// Allows an agent to get all notes for a specific case.
+    /// </summary>
+    [HttpGet("{caseNumber}/notes")]
+    [Authorize(Roles = "Read-Only,Operator,Supervisor,Admin")]
+    [ProducesResponseType(typeof(IReadOnlyList<CaseNoteDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCustomerCaseNotes([FromRoute] string caseNumber)
+    {
+        var query = new GetCaseNotesQuery(caseNumber);
+        
+        return HandleResult(await Mediator.Send(query));
+    }
+    
+    /// <summary>
+    /// Creates a new case with the provided details. Returns the created
+    /// case number.
+    /// </summary>
     [HttpPost]
     [Authorize(Roles = "Operator,Supervisor,Admin")]
     [ProducesResponseType(StatusCodes.Status201Created)]
