71de0d1cddc05e348a0e4f7d388111dcbf62841f
Papp Csongor <csoni2002@gmail.com>
2026-08-06 19:18:31 +0200

    feat: add handler logic to handler entity

--- src/SwiftParcel.Domain/Entities/Handler.cs
+++ src/SwiftParcel.Domain/Entities/Handler.cs
@@ -1,14 +1,98 @@
+using System.Runtime.InteropServices.JavaScript;
+using SwiftParcel.Domain.Exceptions;
+
 namespace SwiftParcel.Domain.Entities;
 
 public class Handler
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public User User { get; set; } = null!;
-    public string Department { get; set; } = string.Empty; //could be an enum
-    public DateTime HireDate { get; set; }
-    public int MaxCases { get; set; }
-    
-    public ICollection<Case> Cases { get; set; } = new List<Case>();
+    public int Id { get; private set; }
+    public int UserId { get; private set; }
+    public User User { get; private set; } = null!;
+    public string Department { get; private set; } = string.Empty; //could be an enum
+    public DateTime HireDate { get; private set; }
+    public int MaxCases { get; private set; }
     public bool IsActive { get; set; } = true;
+
+    private readonly List<Case> _cases = new();
+    public IReadOnlyCollection<Case> Cases => _cases.AsReadOnly();
+
+    public Handler()
+    { }
+    
+    public Handler(int userId, string department, DateTime hireDate, int maxCases)
+    {
+        UserId = userId;
+        Department = department;
+        HireDate = hireDate;
+        MaxCases = maxCases;
+    }
+    
+    public Handler(int id,int userId, string department, DateTime hireDate, int maxCases)
+    {
+        Id = id;
+        UserId = userId;
+        Department = department;
+        HireDate = hireDate;
+        MaxCases = maxCases;
+    }
+    
+    public void UpdateHandler(int userId, 
+        string department, 
+        DateTime hireDate, 
+        int maxCases,
+        bool isActive)
+    {
+        UserId = userId; //todo: validate this
+        ChangeDepartment(department);
+        HireDate = hireDate; //todo: validate this
+        ChangeCapacity(maxCases);
+        if(isActive)
+        {
+            Activate();
+        }
+        else
+        {
+            Deactivate();
+        }
+    }
+    public void ChangeDepartment(string newDepartment)
+    {
+        if (string.IsNullOrWhiteSpace(newDepartment))
+            throw new ArgumentException("Department cannot be empty."
+                , nameof(newDepartment));
+
+        Department = newDepartment;
+    }
+
+    public void ChangeCapacity(int newMaxCases)
+    {
+        if (newMaxCases < _cases.Count)
+            throw new InvalidOperationException(
+                "Capacity cannot be lower than current active cases of a handler.");
+
+        MaxCases = newMaxCases;
+    }
+    
+    public void Activate() => IsActive = true;
+
+    public void Deactivate()
+    {
+        if (_cases.Count > 0)
+            throw new InvalidOperationException(
+                "Handler with active cases cannot be deactivated.");
+
+        IsActive = false;
+    }
+    
+    public bool CanAssignCase() => _cases.Count < MaxCases;
+    
+    public void AssignCase(Case @case)
+    {
+        if (!CanAssignCase())
+            throw new HandlerCapacityExceededException();
+
+        _cases.Add(@case);
+    }
+    
+    
 }
