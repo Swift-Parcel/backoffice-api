4ae6e2df96ba1b980bd4be62258752cd15fad178
Papp Csongor <csoni2002@gmail.com>
2026-08-06 19:19:36 +0200

    refactor: remove codes from domain exceptions

--- src/SwiftParcel.Domain/Exceptions/ParcelNotFoundException.cs
+++ src/SwiftParcel.Domain/Exceptions/ParcelNotFoundException.cs
@@ -5,8 +5,7 @@ namespace SwiftParcel.Domain.Exceptions;
 public class ParcelNotFoundException : DomainException
 {
     public ParcelNotFoundException(string trackingNumber)
-        : base("parcel_not_found", 
-            $"Parcel with tracking number '{trackingNumber}' was not found.", 
+        : base($"Parcel with tracking number '{trackingNumber}' was not found.", 
             HttpStatusCode.NotFound)
     {
     }
