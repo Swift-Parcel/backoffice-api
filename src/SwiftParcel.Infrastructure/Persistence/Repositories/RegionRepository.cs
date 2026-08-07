689e89a3c8fda2c0fe5e0f6946b1fa582dab98b2
Papp Csongor <csoni2002@gmail.com>
2026-08-07 18:53:35 +0200

    chore: add new methods to interface

---
+++ src/SwiftParcel.Infrastructure/Persistence/Repositories/RegionRepository.cs
@@ -0,0 +1,7 @@
+using SwiftParcel.Application.Common.Interfaces.Repositories;
+
+namespace SwiftParcel.Infrastructure.Persistence.Repositories;
+
+public class RegionRepository : IRegionRepository
+{
+}
