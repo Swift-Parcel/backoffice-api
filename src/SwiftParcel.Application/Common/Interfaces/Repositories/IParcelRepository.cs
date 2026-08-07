f380e99058d10aa3806087ea6469725347df91ba
Barnabas Kiss <barnikissbarni@gmail.com>
2026-08-07 11:30:17 +0200

    feature: IParcelRepository

---
+++ src/SwiftParcel.Application/Common/Interfaces/Repositories/IParcelRepository.cs
@@ -0,0 +1,12 @@
+namespace SwiftParcel.Application.Common.Interfaces.Repositories;
+
+using SwiftParcel.Domain.Entities;
+
+public interface IParcelRepository
+{
+    Task<Parcel?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<Parcel?> GetByTrackingNumberAsync(string trackingNumber, CancellationToken cancellationToken = default);
+    Task AddAsync(Parcel parcel, CancellationToken cancellationToken = default);
+    Task UpdateAsync(Parcel parcel, CancellationToken cancellationToken = default);
+    Task DeleteAsync(Parcel parcel, CancellationToken cancellationToken = default);
+}
