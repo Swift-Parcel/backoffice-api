b58f395ccbbe0a846a5e30570a7f2665c8c54e67
Tamás Horneczki <t.horneczki@tcs.com>
2026-08-07 12:47:18 +0200

    chore: add handler repository to dependency injection

--- src/SwiftParcel.Infrastructure/DependencyInjection.cs
+++ src/SwiftParcel.Infrastructure/DependencyInjection.cs
@@ -66,6 +66,7 @@ public static class DependencyInjection
         services.AddScoped<TestDataSeeder>();
         
         services.AddScoped<IHandlerRepository, HandlerRepository>();
+        services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ICaseNumberGenerator, CaseNumberGenerator>();
         services.AddScoped<IParcelNumberGenerator, ParcelNumberGenerator>();
         services.AddScoped<IDeliveryEstimationService, DeliveryEstimationService>();
