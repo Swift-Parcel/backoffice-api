48ebe2e7f972843f421f23e9fd92613e254f58b0
Tamás Horneczki <t.horneczki@tcs.com>
2026-08-10 11:43:40 +0200

    fix: disable users (admins) from deactivating and activating themselves

--- src/SwiftParcel.Application/Users/Commands/UpdateUserStatus/UpdateUserStatusCommandHandler.cs
+++ src/SwiftParcel.Application/Users/Commands/UpdateUserStatus/UpdateUserStatusCommandHandler.cs
@@ -1,11 +1,12 @@
 using MediatR;
+using SwiftParcel.Application.Common.Interfaces;
 using SwiftParcel.Application.Common.Interfaces.Repositories;
 using SwiftParcel.Application.Common.Models;
 using SwiftParcel.Domain.Shared;
 
 namespace SwiftParcel.Application.Users.Commands.UpdateUserStatus;
 
-public class UpdateUserStatusCommandHandler(IUserRepository userRepository) 
+public class UpdateUserStatusCommandHandler(IUserRepository userRepository, ICurrentUserService currentUserService) 
     : IRequestHandler<UpdateUserStatusCommand, Result>
 {
     public async Task<Result> Handle(UpdateUserStatusCommand request, CancellationToken cancellationToken)
@@ -15,6 +16,11 @@ public class UpdateUserStatusCommandHandler(IUserRepository userRepository)
         if (user == null)
         {
             return Result.Failure(Error.NotFound("The specified user does not exist."));
+        }
+
+        if ((int) currentUserService.UserId == user.Id)
+        {
+            return Result.Failure(Error.Forbidden("You cannot update the current user."));
         }
 
         if (user.IsActive == request.IsActive)
